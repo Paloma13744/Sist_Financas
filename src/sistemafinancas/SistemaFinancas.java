0c1fcce33042db55b16be857fefe85049f63c40e
Paloma Nascimento <paloma13744@gmail.com>
2024-10-09 15:28:56 -0300

    Adicionando crud

--- src/sistemafinancas/SistemaFinancas.java
+++ src/sistemafinancas/SistemaFinancas.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package sistemafinancas;
 
 /**
@@ -10,9 +7,6 @@ package sistemafinancas;
  */
 public class SistemaFinancas {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
